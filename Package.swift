2feeb23cd6faf98abd66a25719f43ac649f18be1
kelvin13 <kelvin13ma@gmail.com>
2022-02-05 20:49:16 -0600

    remove argumentparser dependency

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .package(url: "https://github.com/kelvin13/swift-json",                 branch: "master"),
         .package(url: "https://github.com/kelvin13/swift-structured-document",  revision: "99ccc36647d11a2654d758df1208e74b6340438c"),
         
-        .package(url: "https://github.com/apple/swift-argument-parser",         from: "1.0.2"),
+    //    .package(url: "https://github.com/apple/swift-argument-parser",         from: "1.0.2"),
         .package(url: "https://github.com/apple/swift-syntax.git",              revision: "swift-5.6-DEVELOPMENT-SNAPSHOT-2022-01-09-a"),
     ],
     targets: 
